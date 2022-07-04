5e9e159ccd1d59c258679e375799bae78e48fe48
suyo9442 <72788139+suyo9442@users.noreply.github.com>
2022-07-04 20:11:04 +0900

    new 슬라이드

--- js/main.js
+++ js/main.js
@@ -4,89 +4,88 @@ let workSliderItem = document.querySelectorAll('#works .slider_item');
 let workIdx = document.querySelectorAll('#works .page-idx');
 let workPrev = document.querySelector('#works .prev');
 let workNext = document.querySelector('#works .next');
-let index = 0;
+let index1 = 0;
 
 // news_Slider
+let newsSlider = document.querySelector('#news .slider_container');
+let newsSliderItem = document.querySelectorAll('#news .slider_item');
+let newsIdx = document.querySelectorAll('#news .page-idx');
+let index2 = 0;
 
 
 
-/// workSlider pagination prev & next
+// workSlider pagination prev & next
 workNext.addEventListener('click', function (e) {
     e.preventDefault();
-    index += 1;
+    index1 += 1;
 
-
-    if (index >= workSliderItem.length) {
+    if (index1 >= workSliderItem.length - 2) {
         workSlider.style.transform = "translateX(0)";
-        index = 0;
-    } else if (index >= 0) {
-        workSlider.style.transform = "translateX(-" + workSlider.clientWidth / workSliderItem.length * index + "px)";
+        index1 = 0;
+    } else if (index1 >= 0) {
+        workSlider.style.transform = "translateX(-" + workSlider.clientWidth / workSliderItem.length * index1 + "px)";
     }
 
-
-    // index = 0 이면 버튼0에 active
+    // index1 = 0 이면 버튼0에 active
     for (let i = 0; i < workIdx.length; i++) {
-        if (index == i) {
+        if (index1 == i) {
             workIdx[i].classList.add('active');
         }
     }
     for (let i = 0; i < workIdx.length; i++) {
-        if (index !== i) {
+        if (index1 !== i) {
             workIdx[i].classList.remove('active');
         }
     }
-
-    // console.log(index)
 });
-
 
 workPrev.addEventListener('click', function (e) {
     e.preventDefault();
-    index -= 1;
+    index1 -= 1;
 
-    if (index <= 0) {
+    if (index1 <= 0) {
         workSlider.style.transform = "translateX(0)";
-        index = 0;
-    } else if (index >= 0) {
-        workSlider.style.transform = "translateX(-" + workSlider.clientWidth / workSliderItem.length * index + "px)";
+        index1 = 0;
+    } else if (index1 >= 0) {
+        workSlider.style.transform = "translateX(-" + workSlider.clientWidth / workSliderItem.length * index1 + "px)";
     }
 
-    // index = 0 이면 버튼0에 active
+    // index1 = 0 이면 버튼0에 active
     for (let i = 0; i < workIdx.length; i++) {
-        if (index == i) {
+        if (index1 == i) {
             workIdx[i].classList.add('active');
         }
     }
     for (let i = 0; i < workIdx.length; i++) {
-        if (index !== i) {
+        if (index1 !== i) {
             workIdx[i].classList.remove('active');
         }
     }
-
-    // console.log(index)
 });
 
 
-/// workSlider pagination
-workIdx.forEach(function (i, a, b) {
-    workIdx[a].addEventListener('click', function (e) {
-        e.preventDefault();
+/// page-idx 공통 함수
+function idxBtn(btns, slider, item) {
+    btns.forEach(function (i, a, b) {
+        btns[a].addEventListener('click', function (e) {
+            e.preventDefault();
+            index1 = a;
 
-        // index와 workIdx 연동
-        index = a;
+            slider.style.transform = "translateX(-" + slider.clientWidth / item.length * a + "px)";
 
-        workSlider.style.transform = "translateX(-" + workSlider.clientWidth / workSliderItem.length * a + "px)";
+            // 모든 요소에 active 제거
+            btns.forEach(function (i) {
+                i.classList.remove('active')
+            })
 
-        // 모든 요소에 active 제거
-        workIdx.forEach(function (i) {
-            i.classList.remove('active')
+            // 클릭 요소에 active 추가
+            this.classList.add('active')
         })
+    });
+}
 
-        // 클릭 요소에 active 추가
-        this.classList.add('active')
-    })
-});
+// work page-idx
+idxBtn(workIdx, workSlider, workSliderItem);
 
-
-
-
+// news page-idx
+idxBtn(newsIdx, newsSlider, newsSliderItem);
