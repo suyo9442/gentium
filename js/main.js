20d6a75b8396c601524e1af944f5f6fb48370dcd
suyo9442 <72788139+suyo9442@users.noreply.github.com>
2022-07-09 18:11:41 +0900

    스크롤이벤트

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-// work_Slider
+//// main_menu
+let mainMenu = document.querySelectorAll('#main .main_menu article');
+let order = 0;
+
+setInterval(function () {
+    // menu의 개수만큼 order + 1
+    if (order < mainMenu.length - 1) {
+        order += 1;
+    } else {
+        order = 0;
+    }
+
+    // 순서대로 active
+    mainMenu.forEach(function (i, a) {
+        if (order == a) {
+            i.classList.add('active');
+        } else {
+            i.classList.remove('active');
+        }
+    })
+}, 2000)
+
+
+//// work_Slider
 let workSlider = document.querySelector('#works .slider_container');
 let workSliderItem = document.querySelectorAll('#works .slider_item');
 let workIdx = document.querySelectorAll('#works .page-idx');
@@ -6,14 +29,6 @@ let workPrev = document.querySelector('#works .prev');
 let workNext = document.querySelector('#works .next');
 let index1 = 0;
 
-// news_Slider
-let newsSlider = document.querySelector('#news .slider_container');
-let newsSliderItem = document.querySelectorAll('#news .slider_item');
-let newsIdx = document.querySelectorAll('#news .page-idx');
-let index2 = 0;
-
-
-//// work_Slider
 // 다음 & 이전 버튼
 workNext.addEventListener('click', function (e) {
     e.preventDefault();
@@ -63,6 +78,7 @@ workPrev.addEventListener('click', function (e) {
     }
 });
 
+
 // 인덱스 버튼
 workIdx.forEach(function (i, a) {
     workIdx[a].addEventListener('click', function (e) {
@@ -82,7 +98,13 @@ workIdx.forEach(function (i, a) {
 });
 
 
+
 //// news_Slider
+let newsSlider = document.querySelector('#news .slider_container');
+let newsSliderItem = document.querySelectorAll('#news .slider_item');
+let newsIdx = document.querySelectorAll('#news .page-idx');
+let index2 = 0;
+
 // 자동 슬라이드
 setInterval(function () {
     if (index2 < newsSliderItem.length - 3) {
@@ -120,3 +142,23 @@ newsIdx.forEach(function (i, a, b) {
         this.classList.add('active')
     })
 });
+
+
+//// 스크롤이벤트
+var winH = window.innerHeight;
+let aniCon = document.querySelectorAll('.scrollAni');
+
+var _scrollAni = function () {
+
+    for (let i = 0; i < aniCon.length; i++) {
+        var aniPos = aniCon[i].getBoundingClientRect().top;
+
+        if (aniPos - winH < -500) {
+            aniCon[i].classList.remove('opacity');
+        } else {
+            aniCon[i].classList.add('opacity');
+        }
+    }
+}
+
+window.addEventListener('scroll', _scrollAni);
